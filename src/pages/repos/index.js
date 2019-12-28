5366610e7aed41dc81f3305f2cc81e6f8f7f13e7
Neal Fennimore <hi@neal.codes>
2019-12-28 18:50:50 -0500

    Update Repos to fetch on mount

--- src/pages/repos/index.js
+++ src/pages/repos/index.js
@@ -1,38 +1,46 @@
-import React from 'react';
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/static-property-placement */
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getRepos, getIsFetching } from 'global/selectors/github/repos';
+import { fetchRepos } from 'global/actions/github/repos';
 import Layout from 'global/components/Layout';
 import Main from 'global/components/Main';
 import RepoSideBar from 'global/components/RepoSideBar';
 
-const Repos = ( {
-    repos,
-    isFetching,
-} ) => {
-    if ( ! repos.length || isFetching ) {
-        return null;
+export class Repos extends PureComponent {
+    static propTypes = {
+        repos: PropTypes.arrayOf( PropTypes.object ),
+        isFetching: PropTypes.bool,
+        fetchRepos: PropTypes.func,
     }
 
-    return (
-        <Layout>
-            <RepoSideBar />
-            <Main center>
-                <h1>Welcome</h1>
-            </Main>
-        </Layout>
-    );
-};
+    componentDidMount() {
+        if ( ! this.props.repos?.length ) {
+            this.props.fetchRepos();
+        }
+    }
 
-Repos.propTypes = {
-    repos: PropTypes.arrayOf( PropTypes.object ),
-    isFetching: PropTypes.bool,
-};
+    render() {
+        return (
+            <Layout>
+                <RepoSideBar />
+                <Main center>
+                    <h1>{ this.props.isFetching ? 'Loading...' : 'Welcome'}</h1>
+                </Main>
+            </Layout>
+        );
+    }
+}
 
 const connector = connect(
     ( state ) => ( {
         repos: getRepos( state ),
         isFetching: getIsFetching( state ),
+    } ),
+    ( dispatch ) => ( {
+        fetchRepos: () => dispatch( fetchRepos() ),
     } ),
 );
 
