79dcedce8623bcc08baf55f97e8d2aa86859f5af
Neal Fennimore <hi@neal.codes>
2020-01-02 21:22:05 -0500

    Fix error when grabbing no repo from fresh state

--- src/global/selectors/github/issues.js
+++ src/global/selectors/github/issues.js
@@ -5,9 +5,9 @@ const getIssuesSlice = ( state ) => state.github.issues.issues;
 const getSortableSlice = ( state ) => state.github.issues.sortable;
 
 export const getIsFetching = createSelector( getIssuesSlice, ( issues ) => issues.isFetching );
-export const getIssuesByRepoId = createSelector( getIssuesSlice, ( issues ) => ( repoId ) => issues?.byRepoId[repoId] );
+export const getIssuesByRepoId = createSelector( getIssuesSlice, ( issues ) => ( repoId ) => issues?.byRepoId?.[repoId] );
 export const getSortable = createSelector( getSortableSlice, ( sortable ) => sortable );
-export const getSortableByRepoId = createSelector( getSortable, ( sortable ) => ( repoId ) => sortable?.byRepoId[repoId] );
+export const getSortableByRepoId = createSelector( getSortable, ( sortable ) => ( repoId ) => sortable?.byRepoId?.[repoId] );
 
 export const getSortedIssuesByRepoId = createSelector(
     [getSortableByRepoId, getIssuesByRepoId],
